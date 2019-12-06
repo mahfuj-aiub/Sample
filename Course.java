41a498a11470afcdbaf217a16b4750f03a6c1df3
mahfuj-aiub <57424753+mahfuj-aiub@users.noreply.github.com>
2019-12-06 20:38:14 +0600

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,77 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class Course {
+
+	private Teacher teacher;
+	public void addTeacher(Teacher t)
+	{
+		
+	}
+	public Teacher getTeacher()
+	{
+		return teacher;
+	}
+	
+	public static void addCourse(String courseName,String id) {
+		Connection con;
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tutorial","root","aiub18@$");
+			String sql="Insert into course (c_name,t_id)values(?,?)";
+			PreparedStatement stat=con.prepareStatement(sql);
+			//stat.setInt(1,2 );
+			stat.setString(1,courseName);
+			stat.setString(2,id);
+			stat.executeUpdate();
+		//	System.out.println("Hi i am here");
+		}   catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	public static void setCourseTopics(String item,String topic) {
+		Connection con;
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tutorial","root","aiub18@$");
+			
+			String sql="Insert into course values(?,?,?,?,?,?)";
+			PreparedStatement stat=con.prepareStatement(sql);
+		//	System.out.println(item+ " "+topic);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+		
+	}
+	public static String getCourseTopics(String str)
+	{
+		Connection con;
+		String cTopics=null;
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tutorial","root","aiub18@$");
+		    Statement mystat=con.createStatement();
+		    ResultSet myrs=mystat.executeQuery("Select topics from course where c_name='"+str+"'");
+		    if(myrs.next()) {
+		    	cTopics= myrs.getString(1);
+		    }
+		   // System.out.println(cTopics);
+		   
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return cTopics;
+
+	    
+	    
+		
+	}
+}
